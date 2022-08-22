ea9b912ca0d8f5502d95bb7e2bb1d4ff106c54f6
holozone <77789087+holozone@users.noreply.github.com>
2022-08-22 18:10:28 +0200

    Update license

--- mmd.h
+++ mmd.h
@@ -1,10 +1,26 @@
+// Hilfsfunktionen
+
+/* This file is part of mmd (multiple motor driver) from holozone.de.
+
+    mmd is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details. 
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 #include <string>
 
-// Hilfsfunktionen
+
 int canonmode(int onoff) {
 	static struct termios oldt, newt;
 	if (onoff == 1) {
