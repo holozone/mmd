d40f341d336acb9587af2a149b89cd0c635cccb8
holozone <77789087+holozone@users.noreply.github.com>
2022-08-22 18:07:52 +0200

    Update license

--- motordriver.h
+++ motordriver.h
@@ -1,6 +1,21 @@
 // motordriver.h
 //
-// mehrere Motorachsen gleichzeitig Fahren
+
+/* This file is part of mmd (multiple motor driver) from holozone.de.
+
+    mmd is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details. 
+*/
+
+
+// mehrere Motorachsen gleichzeitig Fahren 
 #include <string>
 #include <math.h>
 #include <pthread.h>
